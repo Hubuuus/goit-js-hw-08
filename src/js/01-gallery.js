10fc7c4f6dcecad416ce80d00804c500386f54ca
Hubert Dulas <111136742+Hubuuus@users.noreply.github.com>
2023-01-13 18:27:58 +0100

    add js files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const mapGallery = galleryItems
+  .map(item => {
+    return `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" /></a>`;
+  })
+  .join('');
+gallery.insertAdjacentHTML('beforeend', mapGallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
 
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+
+const player = new Player('vimeo-player', {
+  id: 236203659,
+});
+let vimeoDuration = 0;
+const throttledFn = throttle(() => {
+  player.getCurrentTime().then(function (seconds) {
+    console.log(seconds);
+    vimeoDuration = seconds;
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(vimeoDuration)
+    );
+  });
+}, 1000);
+
+player.on('timeupdate', throttledFn);
+if (localStorage.hasOwnProperty('videoplayer-current-time') === true) {
+  let currentTime = localStorage.getItem('videoplayer-current-time');
+  let parsedTime = JSON.parse(currentTime);
+  player.setCurrentTime(parsedTime);
+}
